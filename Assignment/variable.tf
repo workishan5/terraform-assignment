68c9849aca4b33999052aded5deb3f1cff79ec95
workishan5 <105703278+workishan5@users.noreply.github.com>
2022-06-06 09:37:43 +0530

    Update variable.tf

--- Assignment/variable.tf
+++ Assignment/variable.tf
@@ -1,9 +1,4 @@
 
-variable "resource_group_name_prefix" {
-  default       = "rg"
-  description   = "Prefix of the resource group name that's combined with a random ID so name is unique in your Azure subscription."
-}
-
 variable "resource_group_location" {
   default       = "westeurope"
   description   = "Location of the resource group."
